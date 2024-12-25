29c714bd66a28c4d643b2dfb30fd0d1c0b3dc2ff
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-12-25 21:00:16 +0530

    Create logging.py

---
+++ Badlogo/logging.py
@@ -0,0 +1,18 @@
+import config
+import logging
+from logging.handlers import RotatingFileHandler
+from pyrogram import filters
+
+
+# store all logs
+logging.basicConfig(
+    format="[%(name)s]:: %(message)s",
+    level=logging.INFO,
+    datefmt="%H:%M:%S",
+    handlers=[
+        RotatingFileHandler("logs.txt", maxBytes=(1024 * 1024 * 5), backupCount=10),
+        logging.StreamHandler(),
+    ],
+)
+
+LOGGER = logging.getLogger("INFO")
