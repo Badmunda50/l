1ce7ff6cddc6a131abbf3ecbd12ceeaeb3b32a14
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-12-25 19:46:52 +0530

    Create __init__.py

---
+++ Badlogo/__init__.py
@@ -0,0 +1,12 @@
+from pyrogram import Client 
+from config import *
+from .logging import LOGGER
+
+# Initialize the bot client here
+app = Client(
+         name="Quiz-bot", 
+         api_id=API_ID,
+         api_hash=API_HASH,
+         bot_token=BOT_TOKEN,
+         plugins=dict(root="Badlogo.plugins")  # Ensure the plugins folder is structured correctly
+)
